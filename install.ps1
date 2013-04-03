2127902521307f02f4719a295d4f1c0fd224029e
Eric Miotto <edymtt@netscape.net>
2013-04-03 20:23:18 +0200

    Created a single script to do update and clean installation

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
 $destinationFolder = "packages"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
+nuget update packages.config -r packages
+rd packages
 nuget install packages.config -o packages
--- update.ps1
+++
@@ -1,3 +0,0 @@
-$destinationFolder = "packages"
-if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
-nuget update packages.config -r packages
