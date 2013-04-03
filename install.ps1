b7dd1cdc5d0ea70fab59872ee8f74146f7622fd0
Eric Miotto <edymtt@netscape.net>
2013-04-03 20:23:56 +0200

    Corrected installation script

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 $destinationFolder = "packages"
 if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
+nuget install packages.config -o packages
 nuget update packages.config -r packages
 rd packages
 nuget install packages.config -o packages
